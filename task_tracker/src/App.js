bbb075336df2ca2333533e443fa3dabd8993428e
Ian Clark <ianclark226@gmail.com>
2021-04-07 18:09:30 -0400

    add task

--- task_tracker/src/App.js
+++ task_tracker/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from '../src/components/header';
 import Tasks from '../src/components/tasks';
 import {useState } from 'react';
+import AddTask from './components/AddTask';
 
 
 function App() {
@@ -26,7 +27,14 @@ function App() {
     }
 ])
 
-// Delete Tak
+// Add Task
+
+const addTask =(task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+const newTask = {id, ...task }
+setTasks([...tasks, newTask]);
+}
+// Delete Task
 
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id));
@@ -34,11 +42,12 @@ const deleteTask = (id) => {
 
 // Toggle Reminder 
 const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id  === id ? { ...task, reminder: !task.reminder} : task))
+  setTasks(tasks.map((task) => task.id  === id ? { ...task, reminder: !task.reminder} : task));
 }
   return (
     <div className = 'container'>
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}  onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
